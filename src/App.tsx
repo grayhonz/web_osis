99288163a3337968998f1397f5ccfb0d82100092
grayhonz <zulfiqar.ridho@gmail.com>
2025-05-07 00:29:48 +0700

    Pertama Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+import Anggota from './components/Anggota'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Navbar />
+     <Anggota />
+
+    </>
+  )
+}
+
+
